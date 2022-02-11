5dba099986e3c026d7139b5d0d33c103c7b9cc44
Jeebon <a.jeebon@gmail.com>
2022-02-11 07:43:06 +0600

    CNA-001: Initialize
    Database connection, product route, model, docker re-configure

--- server/app.js
+++ server/app.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const productRoutes = require("./routes/product.routes");
 const app = express();
+const mongodb = require("./mongodb/mongodb.connect");
+mongodb.connect();
 
+app.use(express.json());
+app.use("/products", productRoutes);
+
+app.use((error, req, res, next) => {
+  res.status(500).json({ message: error.message });
+});
+
+app.get("/", (req, res) => {
+  res.json("Hello world!");
+});
 
 module.exports = app;
